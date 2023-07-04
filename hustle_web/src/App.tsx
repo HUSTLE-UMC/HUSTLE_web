7c61058de487ef2914bee0654c6b7fec14e70ec8
onejuneee <onejuneee721@gmail.com>
2023-07-04 20:02:06 +0900

    Docs: 라우팅 구조 생성

--- hustle_web/src/App.tsx
+++ hustle_web/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+const loading = <div>화면을 불러오는 중 입니다.</div>;
+
+// Containers
+const DefaultLayout = React.lazy(() => import("./layout/DefaultLayout"));
+
+// Pages
+const Login = React.lazy(() => import("./pages/login"));
+const Page404 = React.lazy(() => import("./pages/page404/Page404"));
+const Page500 = React.lazy(() => import("./pages/page500/Page500"));
+
+//컴포넌트
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Suspense fallback={loading}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/404" element={<Page404 />} />
+            <Route path="/500" element={<Page500 />} />
+            <Route path="/*" element={<DefaultLayout />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
   );
-}
+};
 
 export default App;
