987734b96920e121ccaf2b745292dd7ad36cd051
yeondu._ <70528332+Yeonsu00-12@users.noreply.github.com>
2023-07-28 22:17:04 +0900

    [Docs] - page 생성 후 컴포넌트 넣어둠

---
+++ hustle_web/src/pages/Forgot/Forgotpage.tsx
@@ -0,0 +1,12 @@
+import ForgotMain from "../../components/Forgot/Forgot";
+import { Container } from "./ForgotStyle";
+
+const FindPage = () => {
+  return (
+    <Container>
+      <ForgotMain/>
+    </Container>
+  )
+}
+
+export default FindPage;
