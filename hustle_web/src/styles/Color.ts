a996740043465ca65f4f21c85981731174357cd6
onejuneee <onejuneee721@gmail.com>
2023-07-31 01:20:12 +0900

    Style: 색상 코드 추가

--- hustle_web/src/styles/Color.ts
+++ hustle_web/src/styles/Color.ts
@@ -2,6 +2,19 @@ const Colors = {
   WriteButton: "#F99E29",
   Text: "#656565",
   MainColor: "#F49E6E",
-};
+  MainSemi: "#FFC06E",
+  MainLight: "#FFDFB6",
+  MainDark: "#FF8F00",
+  MainMild: "#FFF7ED",
+
+  Gray10: "#F0F0F0",
+  Gray20: "#DFDFDF",
+  Gray50: "#999797",
+  Gray70: "#707070",
+  Gray80: "#414141",
+  MainBlack: "#252525",
+  TrueBlack: "#000000",
+  TrueWhite: "#FFFFFF",
+} as const;
 
 export default Colors;
