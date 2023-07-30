26406024c62739791201e9e16a81bdceda28bf2f
onejuneee <onejuneee721@gmail.com>
2023-07-31 02:46:52 +0900

    Refactor: Content 뷰 위치 조정

--- hustle_web/src/components/Content/Styles.tsx
+++ hustle_web/src/components/Content/Styles.tsx
@@ -2,10 +2,12 @@ import styled from "styled-components";
 import Colors from "../../styles/Color";
 
 export const Content = styled.div`
+  width: 1280px;
   height: 100vh;
   display: flex;
   flex-direction: column;
   background-color: ${Colors.Gray10};
-  align-items: normal;
+  align-items: center;
+  margin: 0 auto;
   //margin 및 padding은 있다면 rem으로??
 `;
