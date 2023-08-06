9a1b83183441fc8e25ebf7a7dfea3926dca3d275
onejuneee <onejuneee721@gmail.com>
2023-08-06 18:35:44 +0900

    Fix: 더보기 화면 padding 삭제

--- hustle_web/src/components/ListInfo/Styles.ts
+++ hustle_web/src/components/ListInfo/Styles.ts
@@ -9,20 +9,20 @@ export const Container = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  padding-left: 10rem;
-  padding-right: 6rem;
   border-top: 1px solid ${Colors.Gray10};
   border-bottom: 1px solid ${Colors.Gray10};
   background: #fff;
 `;
 
 export const TitleLabel = styled.h2`
+  margin-left: 6rem;
   font-size: ${FONT.SIZE.TITLE3};
   font-weight: ${FONT.WEIGHT.BOLD};
   color: ${Colors.MainBlack};
 `;
 
 export const NavigateLabel = styled.h2`
+  margin-right: 6rem;
   font-size: ${FONT.SIZE.BODY2};
   font-weight: ${FONT.WEIGHT.REGULAR};
   color: ${Colors.Impact};
