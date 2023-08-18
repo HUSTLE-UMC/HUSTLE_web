ac384a2a05e780deae9c569421d77f2f60d1adf4
onejuneee <onejuneee721@gmail.com>
2023-08-18 17:26:37 +0900

    Feat: 대회 자세히 보기 화면 수정

--- hustle_web/src/pages/Competition/CompetitionApply/Styles.ts
+++ hustle_web/src/pages/Competition/CompetitionApply/Styles.ts
@@ -1,39 +1,63 @@
 import styled from 'styled-components';
 import Colors from '../../../styles/Color';
-// import FONT from '../../styles/Font';
+import FONT from '../../../styles/Font';
 
 export const ApplyContainer = styled.div`
   width: 100%;
-  height: 100vh;
+  height: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
 export const ApplyWrap = styled.div`
-  width: 118rem;
-  height: 129rem;
+  width: auto;
+  height: 14.8rem;
   display: flex;
-  flex-direction: column;
   align-items: center;
   background-color: white;
 `;
 
 export const ContentWrap = styled.div`
   width: 110rem;
-  height: 60.2rem;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  //background-color: white;
+`;
+
+export const DetailBox = styled.div`
+  width: 60.6rem;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+`;
+
+export const TeaminfoLabel = styled.div`
+  width: 60.6rem;
+  height: 5rem;
+  display: flex;
+  align-items: center;
+  background-color: white;
+  font-size: ${FONT.SIZE.BODY2};
+  font-weight: ${FONT.WEIGHT.BOLD};
+  color: ${Colors.MainBlack};
+  font-family: Pretendard;
+`;
+
+export const TeamInfoBox = styled.div`
+  width: 60.6rem;
+  height: 17.7rem;
   display: flex;
   flex-direction: row;
   background-color: white;
-  margin-bottom: 6.6rem;
 `;
 
 export const ImgWrap = styled.img`
   width: 42.6rem;
-  height: 60.2rem;
+  height: 63rem;
   display: flex;
   margin-right: 8.7rem;
-  flex-direction: column;
 `;
 
 export const BtnWrap = styled.div`
