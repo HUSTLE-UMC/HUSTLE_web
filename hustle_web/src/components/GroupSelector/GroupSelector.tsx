26e06774f86b2c5b3e57458926592b87272737f2
onejuneee <onejuneee721@gmail.com>
2023-08-20 19:36:32 +0900

    Fix: 경기 컴포넌트 type 변경

--- hustle_web/src/components/GroupSelector/GroupSelector.tsx
+++ hustle_web/src/components/GroupSelector/GroupSelector.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import * as S from './Styles';
 import { GroupSelectorProps } from './GroupSelectorProps';
 
-const GroupSelector: React.FC<GroupSelectorProps> = ({
-  type = 'Preliminary',
+const GroupSelector = ({
+  type,
   label,
+  selected,
   onClick
-}) => {
+}: GroupSelectorProps) => {
   const ButtonComponent = type === 'Preliminary' ? S.ButtonPre : S.ButtonFinal;
 
   return <ButtonComponent onClick={onClick}>{label}</ButtonComponent>;
