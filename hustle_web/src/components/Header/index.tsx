ee6694bedaa35293eb6097b0fd00b93183180b3f
onejuneee <onejuneee721@gmail.com>
2023-08-21 05:37:30 +0900

    Fix: 헤더 중복 요소 제거

--- hustle_web/src/components/Header/index.tsx
+++ hustle_web/src/components/Header/index.tsx
@@ -41,10 +41,9 @@ const Header = () => {
           커뮤니티
         </H.HeaderBtn>
         <H.HeaderBtn onClick={() => navigate('/ranking')}>순위</H.HeaderBtn>
-        {user !== null ? <LoginComponent user={user} /> : <NotLoginComponent />}
-        <H.HeaderBtn onClick={() => navigate('/mypage')}>Mypage</H.HeaderBtn>
-
-        <H.HeaderBtn>순위</H.HeaderBtn>
+        <H.HeaderBtn onClick={() => navigate('/mypage')}>
+          마이페이지
+        </H.HeaderBtn>
       </H.BtnWrap>
     </H.Header>
   );
