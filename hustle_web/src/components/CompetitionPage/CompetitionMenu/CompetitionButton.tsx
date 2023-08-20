c7145d3a768f75be39e3677839fb4f4d25ddd812
onejuneee <onejuneee721@gmail.com>
2023-08-20 19:36:05 +0900

    Fix: 대회 관련 recoil 설정 변경

---
+++ hustle_web/src/components/CompetitionPage/CompetitionMenu/CompetitionButton.tsx
@@ -0,0 +1,47 @@
+import * as S from './Styles';
+import GroupSelector from '../../GroupSelector/GroupSelector';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import {
+  competitionMenuState,
+  competitionSelectState,
+  competitionTypes
+} from '../../../recoil/CompetitionPage/CompetitionMenu';
+
+interface Props {
+  disable?: boolean;
+}
+
+export const SportsMenu = ({ disable }: Props) => {
+  const [menus, setMenus] = useRecoilState(competitionMenuState);
+  const setIsSelected = useSetRecoilState(competitionSelectState);
+  const handleClick = (id: number) => {
+    setMenus(
+      menus.map((m: competitionTypes) => {
+        return m.id === id
+          ? { ...m, selected: true }
+          : { ...m, selected: false };
+      })
+    );
+    setIsSelected(true);
+  };
+  return (
+    <S.CategoryLayout>
+      {menus.map((m, index) => (
+        <GroupSelector
+          key={index}
+          label={m.label}
+          selected={m.selected}
+          onClick={
+            disable
+              ? () => {
+                  alert('종목 변경을 원하시면 목록 페이지로 돌아가 주세요.');
+                }
+              : () => handleClick(index)
+          }
+        ></GroupSelector>
+      ))}
+    </S.CategoryLayout>
+  );
+};
+
+export default SportsMenu;
