f3e8d04908749974620f0ff903ed76b7b3b1fdc5
choiyoubin <48716638+choiyoubin@users.noreply.github.com>
2023-08-21 18:17:08 +0900

    Feat88: 메인화면 랭킹 추가

--- hustle_web/src/pages/Home/Styles.tsx
+++ hustle_web/src/pages/Home/Styles.tsx
@@ -61,3 +61,32 @@ export const TextBox = styled.div`
   font-size: ${FONT.SIZE.BODY3};
   color: ${Colors.Gray80};
 `;
+
+export const Box = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const List = styled.div`
+  display: flex;
+  padding: 10px;
+  width: 862px;
+`;
+
+export const sub1 = styled.span`
+  width: auto;
+  margin: 2.5px 0;
+  font-size: 15px;
+  color: #000;
+  flex: 9;
+  text-align: left;
+`;
+
+export const sub2 = styled.span`
+  width: 5.5%;
+  margin: 5.5px 0;
+  font-size: ${FONT.SIZE.BODY2};
+  font-weight: ${FONT.WEIGHT.BOLD};
+  color: #000;
+  margin-left: 1.5rem;
+`;
