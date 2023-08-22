354d1d51f7f2b45ac37b8a9225a1bfd83a821973
yeondu._ <70528332+Yeonsu00-12@users.noreply.github.com>
2023-08-23 06:51:43 +0900

    [Fix] - 카카오로그인 수정

--- hustle_web/src/components/Login/Kakao/KakaoLogin.tsx
+++ hustle_web/src/components/Login/Kakao/KakaoLogin.tsx
@@ -1,12 +1,41 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const kakaoLogin = () => {
-  const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API;
-  const REDIRECT_URL = process.env.REACT_APP_KAKAO_REDIRECT_URL;
-  const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URL}&response_type=code`
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get('code');
+    const grantType = 'authorization_code';
+    const REST_API_KEY = process.env.REACT_API_KEY;
+    const REDIRECT_URI = process.env.REDIRECT_URI;
+    
+    axios.post(
+      `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`,
+      {},
+      { headers: { 'Content-type': 'application/x-www-form-urlencoded;charset=utf-8' }}
+    )
+    .then((res) => {
+      console.log(res);
+      const accessToken = res.data.accessToken;
+      axios.post('https://api.sport-hustle.com/api/auth/signin/oauth',
+      {},
+      {
+        headers : {
+          Authorization : `Bearer ${accessToken}`,
+          'Content-type' : 'application/x-www-form-urlencoded;charset=utf-8',
+        }
+      })
+      .then((res: any) => {
+        console.log('seconde : ', res.data.accessToken);
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  },[])
 
-  window.location.href = kakaoUrl;
   return (
-    <div>kakao login 이동중 ...
+    <div>
+      로그인 중입니다. 잠시만 기다려주세요
     </div>
   );
 };
